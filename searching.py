00a8975ec5b3dc02beb8f286e6a24b5633ac891b
nelavanickova <nela.1515@seznam.cz>
2026-04-16 15:47:30 +0200

    treti  kontrola

--- searching.py
+++ searching.py
@@ -28,15 +28,35 @@ def linear_search(sekvence, cislo):
             pozice.append(index+1)
             slovnik["positions"] = pozice
             slovnik["count"] = scitac
+
     return slovnik
+
+def binary_search(sekvence, cislo):
+    leva = 0
+    prava = len(sekvence) - 1
+    pozice = []
+    while leva <= prava:
+        stred = (leva + prava) // 2
+        if sekvence[stred] == cislo:
+            return stred
+        elif sekvence[stred] > cislo:
+            prava = stred -1
+        else:
+            leva = stred + 1
+    return None
 
 
 
 def main():
     nacteni = (read_data("sequential.json", "unordered_numbers"))
-    linearni =(linear_search( nacteni, -10))
-    print(nacteni)
-    print(linearni)
+    nacteno_serazeno =(read_data("sequential.json", "ordered_numbers"))
+    linearni = (linear_search( nacteni, 0))
+    binarni = binary_search(nacteno_serazeno, -3)
+
+    # print(nacteni)
+    # print(linearni)
+    print(binarni)
+
 
 if __name__ == '__main__':
     main()
