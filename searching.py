73b8280c4c83c70ed286d713e656fa8f52535245
nelavanickova <nela.1515@seznam.cz>
2026-04-16 15:12:57 +0200

    druha  kontrola

--- searching.py
+++ searching.py
@@ -11,17 +11,32 @@ def read_data(file_name, field):
     :param file_name: (str), name of json file
     :param field: (str), field of a dict to return
     :return: (list, string),
+        file_path = os.path.join(cwd_path, file_name)
     """
     with open(file_name, "r") as file:
         data = json.load(file)
-        s_data = data[field]
-    return s_data
-    file_path = os.path.join(cwd_path, file_name)
-print(read_data("sequential.json", "dna_sequence"))
+        sequential_data = data[field]
+    return sequential_data
+
+def linear_search(sekvence, cislo):
+    scitac = 0
+    pozice = []
+    slovnik = {}
+    for index, prvek in enumerate(sekvence):
+        if prvek == cislo:
+            scitac += 1
+            pozice.append(index+1)
+            slovnik["positions"] = pozice
+            slovnik["count"] = scitac
+    return slovnik
+
+
 
 def main():
-    pass
-
+    nacteni = (read_data("sequential.json", "unordered_numbers"))
+    linearni =(linear_search( nacteni, -10))
+    print(nacteni)
+    print(linearni)
 
 if __name__ == '__main__':
     main()
